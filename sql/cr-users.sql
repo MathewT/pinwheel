c963e0ab9a24e13afa0c1cb0b04e807a1e008025
Mathew Thomas <mathew.thomas@longdivision.com>
2018-05-26 12:22:43 -0400

    add db stuff
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- sql/cr-users.sql
+++ sql/cr-users.sql
@@ -1,5 +1,9 @@
 
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
  ID serial NOT NULL PRIMARY KEY,
  info json NOT NULL
 );
+
+SELECT * FROM pg_catalog.pg_tables WHERE schemaname = 'public';
--- sql/run-sql.sh
+++ sql/run-sql.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo 'helo sql world.'
+
+cd /sql
+env |sort
+
+psql postgresql://postgres:swordfish@db0.local:5432 -f ./cr-users.sql
+psql postgresql://postgres:swordfish@db0.local:5432 
