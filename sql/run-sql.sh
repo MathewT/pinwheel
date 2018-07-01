f2f40a7d291ccc4913bb7200991bdcf28c68aab8
Mathew Thomas <mathew.thomas@longdivision.com>
2018-07-01 16:16:08 -0400

    sql
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- sql/run-sql.sh
+++ sql/run-sql.sh
@@ -5,5 +5,6 @@ echo 'helo sql world.'
 cd /sql
 env |sort
 
-psql postgresql://postgres:swordfish@db0.local:5432 -f ./cr-users.sql
-psql postgresql://postgres:swordfish@db0.local:5432 
+## psql postgresql://postgres:swordfish@db0.local:5432 -f ./cr-users.sql
+# psql postgresql://postgres:swordfish@db0.local:5432 
+psql postgresql://postgres:swordfish@localhost:5432 
